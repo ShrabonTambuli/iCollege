5d062c444a641019c3c325c8025e58c0cd3403fc
ShrabonTambuli <shrabonsa@gmail.com>
2023-07-23 22:41:55 +0600

    update image

--- src/Components/Home/Achievement.jsx
+++ src/Components/Home/Achievement.jsx
@@ -9,7 +9,7 @@ const Achievement = () => {
             <div className="grid grid-cols-1 lg:grid-cols-3 gap-2 mt-14">
                 <div className="card w-96 h-96 bg-base-100 shadow-xl">
                     <figure className="px-10 pt-10">
-                        <img src="../../../public/image/star.jpg" alt="Shoes" className="rounded-xl" />
+                        <img src="/image/star.jpg" alt="Shoes" className="rounded-xl" />
                     </figure>
                     <div className="card-body items-center text-center">
                         <h2 className="card-title">Star Award!</h2>
@@ -17,7 +17,7 @@ const Achievement = () => {
                 </div>
                 <div className="card w-96 h-96 bg-base-100 shadow-xl">
                     <figure className="px-10 pt-10">
-                        <img src="../../../public/image/ascer.jpg" alt="Shoes" className="rounded-xl" />
+                        <img src="/image/ascer.jpg" alt="Shoes" className="rounded-xl" />
                     </figure>
                     <div className="card-body items-center text-center">
                         <h2 className="card-title">Oscar Award!</h2>
@@ -25,7 +25,7 @@ const Achievement = () => {
                 </div>
                 <div className="card w-96 h-96 bg-base-100 shadow-xl">
                     <figure className="px-10 pt-10">
-                        <img src="../../../public/image/success.avif" alt="Shoes" className="rounded-xl" />
+                        <img src="/image/success.avif" alt="Shoes" className="rounded-xl" />
                     </figure>
                     <div className="card-body items-center text-center">
                         <h2 className="card-title">100% Success!</h2>
